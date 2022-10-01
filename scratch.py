60d150443f7280676eaa93272d122ddad7ed02c8
curtis-roy <curtis.roy@libertymutual.com>
2022-10-01 12:38:54 -0700

    Add work on astronomy lab.

--- scratch.py
+++ scratch.py
@@ -1,39 +1,28 @@
 #!/bin/env python3
 
-# """Scratchpad"""
+"""Scratchpad"""
 
-from pprint import pprint
+import datetime
 import requests
+from requests.auth import HTTPBasicAuth
+from constants import constant1, constant2
 import json
 
+#eochair = constant1
+#runda = constant2
 
-def get_location():
-    """ Returns the longitude and latitude for the location of an IP.
+def get_sun_position(latitude, longitude):
 
-        Returns:
-        float:  latitude
-        float:  longitude
-        """
-    target = requests.get('http://ip-api.com/json/174.31.14.127?fields=lat,lon')
-    target_data = json.loads(target.text)
-    latitude = (target_data['lat'])
-    longitude = (target_data['lon'])
-    return latitude, longitude
-
-get_location()
+    astronomy_api = ("https://api.astronomyapi.com/api/v2/bodies/positions/sun")
+    response = requests.get(astronomy_api, auth=HTTPBasicAuth(constant1, constant2))
+    response_data = json.loads(response.text)
+    print(response_data)
 
 
-
-    # add return values here when available
-    # return None, None
-
-# def get_sun_position(latitude, longitude):
-#     # future enhancement - add options for other "local" bodies in our solar system. input? menu option?
-#     #  
-#     now = datetime.now().strftime("%H:%M:%S")
-#     today = date.today()
-#     auth = ASTRONOMYAPI_ID,ASTRONOMYAPI_SECRET
-#     # requirements for the API
+    #now = datetime.now().strftime("%H:%M:%S")
+    # today = date.today()
+    
+    # # requirements for the API
 #     payload = { "latitude" : float(latitude),
 #                 "longitude" : float(longitude),
 #                 "elevation" : "50", # might or might not be required
@@ -51,3 +40,21 @@ get_location()
 
 #     # second one is the non broken apart its for the altitude, you'd just change altitude to azimuth
 #     # response.json()["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"]["altitude"]["degrees"]
+
+
+
+# def get_location():
+#     """ Returns the longitude and latitude for the location of an IP.
+
+#         Returns:
+#         float:  latitude
+#         float:  longitude
+#         """
+#     target = requests.get('http://ip-api.com/json/174.31.14.127?fields=lat,lon')
+#     target_data = json.loads(target.text)
+#     latitude = (target_data['lat'])
+#     longitude = (target_data['lon'])
+#     return latitude, longitude
+
+# get_location()
+#get_location is now working and returns the desired values.  Copied into solar.py
--- solar.py
+++ solar.py
@@ -8,56 +8,58 @@
 import datetime
 import requests
 from constants import constant1, constant2
+import json
 
 eochair = constant1
 runda = constant2
+
+def get_location():
+    """ Returns the longitude and latitude for the location of an IP.
+
+        Returns:
+        float:  latitude
+        float:  longitude
+        """
+    target = requests.get('http://ip-api.com/json/174.31.14.127?fields=lat,lon')
+    target_data = json.loads(target.text)
+    latitude = (target_data['lat'])
+    longitude = (target_data['lon'])
+    return latitude, longitude
+
+get_location()
 
 ###############################
 ####  PICK IT BACK UP HERE ####
 ###############################
 
-def get_location():
-    """ Returns the longitude and latitude for the location of this IP.
-        
-        Returns:
-        str:  latitude
-        str:  longitude
-        """
-    response = requests.get(f'http://ip-api.com/json/174.31.14.127?fields=status,message,regionName,city,lat,lon,timezone,offset,query')
-    response_dict = response.json()
+# def get_sun_position(latitude, longitude):
+#     """Returns the current position of the sun in the sky at the specified location.
 
+#     Parameters:
+#     latitude (str)
+#     longitude (str)
 
-    # add return values here when available
-    return None, None
+#     Returns:
+#     float: azimuth
+#     float: altitude
+#     """
 
-def get_sun_position(latitude, longitude):
-    """Returns the current position of the sun in the sky at the specified location.
+#     # add return values here when available
+#     return 0, 0
 
-    Parameters:
-    latitude (str)
-    longitude (str)
-
-    Returns:
-    float: azimuth
-    float: altitude
-    """
-
-    # add return values here when available
-    return 0, 0
-
-def print_position(azimuth, altitude):
-    """Prints the position of the sun in the sky using the supplied coordinates
+# def print_position(azimuth, altitude):
+#     """Prints the position of the sun in the sky using the supplied coordinates
     
-    Parameters:
-    azimuth (float)
-    altitude (float)
-    """
+#     Parameters:
+#     azimuth (float)
+#     altitude (float)
+#     """
 
-    print('The Sun is currently at: ')
+#     print('The Sun is currently at: ')
 
-if __name__ == "__main__":
-    latitude, longitude = get_location()
-    azimuth, altitude = get_sun_position()
-    print_position(azimuth, altitude)
+# if __name__ == "__main__":
+#     latitude, longitude = get_location()
+#     azimuth, altitude = get_sun_position()
+#     print_position(azimuth, altitude)
 
 

ea357dfccf475d51b67f24b2b948a4ab9d0b5bc4
curtis-roy <curtis.roy@libertymutual.com>
2022-10-10 10:47:43 -0700

    Add updates to scratch.py

--- scratch.py
+++ scratch.py
@@ -3,19 +3,24 @@
 """Scratchpad"""
 
 import datetime
+import time
 import json
 import requests
 from constants import constant1, constant2
 
 def get_location():
-    """ Returns the longitude and latitude for the location of an IP.
+    """ Gathers the longitude and latitude for the location of an IP.
+        Parameter:
+        IP address
 
         Returns:
         float:  latitude
         float:  longitude
-        """
-    # current IP is a local entry for accuracy
-    # URL format based on the value returned by the API
+
+        Note:  The IP below in "target" is a local entry for accuracy.
+        The URL format is based on the value required by the API.
+        If this is run from a VM, it will return the location of the data center.
+    """
     target = requests.get('http://ip-api.com/json/174.31.14.127?fields=lat,lon')
     target_data = json.loads(target.text)
     latitude = (target_data['lat'])
@@ -24,30 +29,37 @@ def get_location():
 
 get_location()
 
-# test lat/lon values:  47.6987, -117.4397 (also, elevation is 526 m)
+###############################################
+### PICK UP HERE:  WORKING ON ELEVATION API ###
+###############################################
+
+def get_elevation():
+    """ Gathers the elevation at a specified latitude / longitude.
+
+        Returns:
+        <type?>:  elevation
+    """
+    lat, lon = get_location()
+    target = requests.get('https://api.open-elevation.com/api/v1/lookup?locations= ## lat, lon ##)
+    target_data = json.loads(target.text)
+    elevation = (target_data["elevation:"])
+    return elevation
+
+# test elevation is 526 m)
 
 def time_params():
 
     """ Gathers the date and time values required for position.
-    
+
     Returns:
     str: today's date
-    str" Current time
+    str: current time
 
     """
-    ##########################################################
-    ### IT'S GETTING THE HOURS, MINUTES, AND SECONDS WRONG ###
-    ##########################################################
-    
-    day_time = datetime.date.today
+
     cal_day = datetime.date.today().strftime("%Y-%m-%d")
-    time_now = datetime.date.today().strftime("%H:%M:%S")
-    print(day_time)
-    print(type(day_time))
-    print(cal_day)
-    print(type(cal_day))
-    print(time_now)
-    print(type(time_now))
+    time_now = time.strftime("%H:%M:%S")
+
     return cal_day, time_now
 
 time_params()
@@ -60,6 +72,7 @@ def get_sun_position():
         Parameters:
         latitude (str)
         longitude (str)
+        elevation (str)
         Returns:
         float:  altitude
         float:  azimuth
@@ -67,11 +80,12 @@ def get_sun_position():
 
     loc_lat, loc_lon = get_location()
     cal_day, time_now = time_params()
+    loc_elev = get_elevation()
     astronomy_api = ("https://api.astronomyapi.com/api/v2/bodies/positions/sun")
     let_me_in = (constant1, constant2)
     values = {  "latitude": loc_lat,
                 "longitude": loc_lon,
-                "elevation": str(526), # in meters
+                "elevation": loc_elev,
                 "from_date": cal_day,
                 "to_date": cal_day,
                 "time": time_now }
@@ -93,8 +107,9 @@ def print_position():
         Returns:
         print statement
     """
+    cal_day, time_now = time_params()
     azimuth, altitude = get_sun_position()
-    return f'The Sun is currently at {azimuth} degrees azimuth and {altitude} degrees altitude.'
+    return f'As of {cal_day} {time_now}, the Sun is at {azimuth} degrees azimuth and {altitude} degrees altitude.'
 
 print_position()
 
@@ -102,9 +117,3 @@ if __name__ == "__main__":
     ip_location = get_location()
     body_position = get_sun_position()
     output = print(print_position())
-
-
-
-# get_sun_position is working with test values.  Need to refine.
-
-# get_location is now working and returns the desired values.  Copied into solar.py

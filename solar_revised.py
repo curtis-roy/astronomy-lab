5c17e23ac74c893d58415c932cbfb14e0d35ba54
curtis-roy <curtis.roy@libertymutual.com>
2022-10-19 16:08:45 -0700

    Add work done on 10-19-2022

--- solar_revised.py
+++ solar_revised.py
@@ -19,17 +19,11 @@ from constants import constant1, constant2
 ### CONTINUE REVISIONS HERE.  ADDRESS PEP8 ISSUES. ###
 ######################################################
 
-parser = argparse.ArgumentParser(
-    description="Find distance of planet or the Sun from current location"
-)
-parser.add_argument(
-    "--at",
+parser = argparse.ArgumentParser(description="Find distance of planet or the Sun from current location")
+parser.add_argument("--at",
     type=datetime.datetime.fromisoformat,
     default=datetime.datetime.now().isoformat(timespec="seconds"),
-    help="""Date and Time, given in YYYY-MM-DD, and HH:MM:SS 24 hour format, separated by a "T", i.e. 2022-10-01T20:30:00""",
-)
-# parser.add_argument('--from', action='store_true', help='You must follow "--from" with a "lat" and "lon" argument')
-# subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands', help='sub-command help')
+    help="""Date and Time, given in YYYY-MM-DD, and HH:MM:SS 24 hour format, separated by a "T", i.e. 2022-10-01T20:30:00""",)
 parser.add_argument("--lat", type=float, default=0.0, help="enter a latitude")
 parser.add_argument("--long", type=float, default=0.0, help="enter a longitude")
 parser.add_argument("body", default="Sun", nargs="?", help="enter an astronomical body")
@@ -45,53 +39,47 @@ astro_body = args.body.capitalize()
 
 
 def get_observer_location():
-    """Returns the longitude and latitude for the geolocation of this machine, based on its IP address."""
-    response = requests.get("http://ip-api.com/json/?fields=lat,lon")
+    """ Returns the longitude and latitude for the geolocation of this machine, based on its IP address.
+        NOTE:  IP address is entered manually below; autodetected IP is the Amazon Workspace machine
+        in a data center, probably in Ashburn VA."""
+    response = requests.get("http://ip-api.com/json/174.31.14.127?fields=lat,lon")
     data = response.json()
     loc_lat = data["lat"]
     loc_lon = data["lon"]
     return loc_lat, loc_lon
 
+get_observer_location()
 
 def get_observer_elevation(loc_lat, loc_lon):
+
     """Returns the elevation for the latitude and longitude previously entered."""
-    response = requests.get(
-        f"https://api.open-elevation.com/api/v1/lookup?locations={loc_lat},{loc_lon}"
-    )
+    response = requests.get(f"https://api.open-elevation.com/api/v1/lookup?locations={loc_lat},{loc_lon}")
     data = response.json()
     loc_elev = data["results"][0]["elevation"]
     return loc_elev
 
+get_observer_elevation()
 
-def get_body_position(
-    latitude, longitude, elevation, astro_body, date_entered, time_entered
-):
+
+def get_body_position(loc_lat, loc_lon, elevation, astro_body, date_entered, time_entered):
     """Returns the current position of the body in the sky at the specified location and date/time entered."""
     params = {
-        "longitude": longitude,
-        "latitude": latitude,
+        "longitude": loc_lon,
+        "latitude": loc_lat,
         "elevation": elevation,
         "from_date": {date_entered},
         "to_date": {date_entered},
         "time": {time_entered},
     }
-    response = requests.get(
-        f"https://api.astronomyapi.com/api/v2/bodies/positions/{astro_body}/",
-        auth=(constant1, constant2),
-        params=params,
-    )
+    response = requests.get(f"https://api.astronomyapi.com/api/v2/bodies/positions/{astro_body}/",
+        auth=(constant1, constant2),params=params,)
     data = response.json()
-    altitude = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"][
-        "altitude"
-    ]["string"]
-    azimuth = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"][
-        "azimuth"
-    ]["string"]
-    from_earth = data["data"]["table"]["rows"][0]["cells"][0]["distance"]["fromEarth"][
-        "km"
-    ]
+    altitude = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"]["altitude"]["string"]
+    azimuth = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"]["azimuth"]["string"]
+    from_earth = data["data"]["table"]["rows"][0]["cells"][0]["distance"]["fromEarth"]["km"]
     magnitude = data["data"]["table"]["rows"][0]["cells"][0]["extraInfo"]["magnitude"]
     return azimuth, altitude, from_earth, magnitude
+get_body_position()
 
 
 def print_position(
